f900452f596567786a175cda490ba7bfc55c0eb2
Anna rozymuradov <annarozymuradov@Annas-MacBook-Air.local>
2020-05-01 05:02:32 -0700

    <2>

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,13 @@ var copyAllButtons = [];
 for (let i=0; i<all_buttons.length; i++) {
     copyAllButtons.push(all_buttons);
 }
+
+function buttonColorChange(buttonThingy) {
+    if (buttonThingy.value === 'red') {
+        buttonsRed();
+    } else if (buttonThingy.value === 'green') {
+        buttonsGreen();
+    } else if (buttonThingy.value === 'reset') {
+        buttonColorReset();
+    }
+}
